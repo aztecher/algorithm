9b1a99d5b6de230d7c65b7df772195ecae410a7a
aztecher <mikiyaf.business@gmail.com>
2023-04-03 08:58:05 +0900

    Fix insertion_sort and add insertion_sort_asc

--- src/sorting/insertion_sort.rs
+++ src/sorting/insertion_sort.rs
@@ -1,18 +1,37 @@
-pub fn insertion_sort<T: Ord + Copy>(input: &mut [T]) {
-    for j in 2..input.len() {
+pub fn insertion_sort<T: Ord + Copy + std::fmt::Debug>(input: &mut [T]) {
+    for j in 1..input.len() {
         let key = input[j];
-        let mut i = j - 1;
-        while j > 0 && input[i] > key {
-            input[i+1] = input[i];
+        // 'i' will be decremented in while loop and 'i' can be '-1' value in this loop.
+        // but while condition requires
+        //   1. 'i' is 0,1,2....n
+        //   2. 'i' is 'usize' type (-1 is not the element of 'usize')
+        // so, we have to...
+        //   1. define i as 'i8' and cast it as usize when it will be used as index
+        //   2. add while condition i >= 0 because the condition i = -1 is no more evalting
+        let mut i = (j - 1) as i8;
+        while j > 0 && i >= 0 && input[(i as usize)] > key {
+            input[((i+1) as usize)] = input[(i as usize)];
             i = i - 1;
         }
-        input[i+1] = key
+        input[((i+1) as usize)] = key
+    }
+}
+
+pub fn insertion_sort_asc<T: Ord + Copy>(input: &mut [T]) {
+    for j in 1..input.len() {
+        let key = input[j];
+        let mut i = (j - 1) as i8;
+        while j > 0 && i >= 0 && input[(i as usize)] < key {
+            input[((i+1) as usize)] = input[(i as usize)];
+            i = i - 1;
+        }
+        input[((i+1) as usize)] = key
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::super::is_sorted;
+    use super::super::{is_sorted,is_sorted_asc};
     use super::*;
 
     #[test]
@@ -20,5 +39,84 @@ mod tests {
         let mut arr: [u8; 0] = [];
         insertion_sort(&mut arr);
         assert!(is_sorted(&arr));
+    }
+
+    #[test]
+    fn empty_asc() {
+        let mut arr: [u8; 0] = [];
+        insertion_sort_asc(&mut arr);
+        assert!(is_sorted_asc(&arr));
+    }
+
+    #[test]
+    fn one_element() {
+        let mut arr: [char; 1] = ['a'];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
+    }
+
+    #[test]
+    fn one_element_asc() {
+        let mut arr: [char; 1] = ['a'];
+        insertion_sort_asc(&mut arr);
+        assert!(is_sorted_asc(&arr));
+    }
+
+    #[test]
+    fn already_sorted() {
+        let mut arr: [&str; 3] = ["a", "b", "c"];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
+    }
+
+    #[test]
+    fn already_sorted_asc() {
+        let mut arr: [&str; 3] = ["c", "b", "a"];
+        insertion_sort_asc(&mut arr);
+        assert!(is_sorted_asc(&arr));
+    }
+
+    #[test]
+    fn basic() {
+        let mut arr: [&str; 4] = ["d", "a", "c", "b"];
+        println!("before = {:?}", arr);
+        insertion_sort(&mut arr);
+        println!("after = {:?}", arr);
+        assert!(is_sorted(&arr));
+    }
+
+    #[test]
+    fn basic_asc() {
+        let mut arr: [&str; 4] = ["a", "d", "c", "b"];
+        insertion_sort_asc(&mut arr);
+        assert!(is_sorted_asc(&arr));
+    }
+
+    #[test]
+    fn odd_number_of_elements() {
+        let mut arr: Vec<&str> = vec!["d", "a", "c", "e", "b"];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
+    }
+
+    #[test]
+    fn odd_number_of_elements_asc() {
+        let mut arr: Vec<&str> = vec!["d", "a", "c", "e", "b"];
+        insertion_sort_asc(&mut arr);
+        assert!(is_sorted_asc(&arr));
+    }
+
+    #[test]
+    fn repeated_elements() {
+        let mut arr: Vec<usize> = vec![542, 542, 542, 542];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&mut arr));
+    }
+
+    #[test]
+    fn repeated_elements_asc() {
+        let mut arr: Vec<usize> = vec![542, 542, 542, 542];
+        insertion_sort_asc(&mut arr);
+        assert!(is_sorted_asc(&mut arr));
     }
 }
--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -14,6 +14,23 @@ pub fn is_sorted<T: std::cmp::PartialOrd>(arr: &[T]) -> bool {
     true
 }
 
+pub fn is_sorted_desc<T: std::cmp::PartialOrd>(arr: &[T]) -> bool {
+    is_sorted(arr)
+}
+
+pub fn is_sorted_asc<T: std::cmp::PartialOrd>(arr: &[T]) -> bool {
+    if arr.is_empty() {
+        return true
+    }
+    let mut prev = &arr[0];
+    for item in arr.iter().skip(1) {
+        if prev < item {
+            return false
+        }
+        prev = item;
+    }
+    true
+}
 
 #[cfg(test)]
 mod tests {
@@ -26,5 +43,16 @@ mod tests {
         assert!(is_sorted(&[0, 1, 1]));
         assert!(!is_sorted(&[1, 0]));
         assert!(!is_sorted(&[2, 3, 1, -1, 5]));
+    }
+
+    #[test]
+    fn is_sorted_asc() {
+        use super::*;
+        assert!(is_sorted_asc(&[] as &[isize]));
+        assert!(is_sorted_asc(&["a"]));
+        assert!(is_sorted_asc(&[3, 2, 1]));
+        assert!(is_sorted_asc(&[1, 1, 0]));
+        assert!(!is_sorted_asc(&[0, 1]));
+        assert!(!is_sorted_asc(&[5, -1, 1, 3, 2]));
     }
 }
